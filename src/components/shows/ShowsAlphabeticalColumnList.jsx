ede55d6e0b7b541acdd1fb173166c32d21f73dd1
Achintha Dilesha <achintha.dilesha@gmail.com>
2022-02-11 11:36:51 +0530

    implement link navigation to shows alphabetical list

--- src/components/shows/ShowsAlphabeticalColumnList.jsx
+++ src/components/shows/ShowsAlphabeticalColumnList.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import shows from '../../data/Shows';
 import ShowsAlphabeticalBannerLetter from './ShowsAlphabeticalBannerLetter';
 import ShowsCategoryBanner from './ShowsCategoryBanner';
+import { Link } from 'react-router-dom';
 
-function ShowsAlphabeticalColumnList({letter}) {
+function ShowsAlphabeticalColumnList({ letter }) {
     return (
-        <div className='pb-[37px]'>
-            {/* leter container here - A */}
-            <ShowsAlphabeticalBannerLetter letter={letter} />
-            {/* shows post container */}
-            <div className='flex pt-[14px] w-screen md:w-9/12 auto-cols-max md:auto-cols-min overflow-x-auto'>
-            {shows.map((item)=>(
-                    <ShowsCategoryBanner key={item.id} showsCover={item.showsCover} showsTitle={item.showsTitle} showsDate={item.showsDate}/>
-                ))}
+        <Link to='/shows/showsdetails'>
+            <div className='pb-[37px]'>
+                {/* leter container here - A */}
+                <ShowsAlphabeticalBannerLetter letter={letter} />
+                {/* shows post container */}
+                <div className='flex pt-[14px] w-screen md:w-9/12 auto-cols-max md:auto-cols-min overflow-x-auto'>
+                    {shows.map((item) => (
+                        <ShowsCategoryBanner key={item.id} showsCover={item.showsCover} showsTitle={item.showsTitle} showsDate={item.showsDate} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
