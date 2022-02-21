db1e88ee460f98fcdd069f757273e8eb6e26c520
Achintha Dilesha <achintha.dilesha@gmail.com>
2022-02-21 13:35:06 +0530

    home screen meet the hosts UI issue fix

--- src/components/home/HomeLeftContainer.jsx
+++ src/components/home/HomeLeftContainer.jsx
@@ -12,7 +12,7 @@ function HomeLeftContainer() {
     return (
         <div className='flex-col ml-[26px] md:ml-[59px]'>
             <div className='mt-[112px] ml-[26px] mr-[26px] mb-[32px] lg:mt-[34px] lg:ml-[59px] lg:mr-[113px] lg:mb-[40px]'>
-                <ImageSlider bannerList={bannerList}/>
+                <ImageSlider bannerList={bannerList} />
             </div>
             <div className='pr-[20px]'>
                 {/* latest episodes */}
@@ -49,19 +49,23 @@ function HomeLeftContainer() {
                 <div className=''>
                     <div className='flex'>
                         <HomeRowContainerTitle rowTitle='Meet The Hosts' />
-                        <div className='flex items-center text-#666666 text-[11px] pt-[7px]'>
-                            <p>Browse All{'>'}{'>'}</p>
-                        </div>
                     </div>
-                    <div className='flex space-x-[55px] md:space-x-[36px] w-full overflow-x-auto'>
-                        {hosts.map((item) => (
-                            <HomeContainerHostCard
-                                key={item.id}
-                                bannerImage={item.hostCover}
-                                bannerTitle={item.hostName}
-                                bannerType={item.host}
-                            />
-                        ))}
+                    <div className='flex-col'>
+                        <div className='flex justify-end mb-[10px]'>
+                            <div className='flex items-center text-#666666 text-[11px] pt-[7px]'>
+                                <p>Browse All{'>'}{'>'}</p>
+                            </div>
+                        </div>
+                        <div className='flex space-x-[55px] md:space-x-[36px] w-full overflow-x-auto'>
+                            {hosts.map((item) => (
+                                <HomeContainerHostCard
+                                    key={item.id}
+                                    bannerImage={item.hostCover}
+                                    bannerTitle={item.hostName}
+                                    bannerType={item.host}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
