7bb67a361d3ee62d61c359f7ca5e06a1823699fc
Achintha Dilesha <achintha.dilesha@gmail.com>
2022-02-21 09:42:52 +0530

    implemented card modules and styling for swiper js

--- src/components/shared/ImageSlider.jsx
+++ src/components/shared/ImageSlider.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards, EffectCoverflow } from 'swiper';
+import { Swiper, SwiperSlide} from 'swiper/react';
+
 import 'swiper/css';
+import 'swiper/css/effect-cards';
+import "swiper/css/effect-coverflow";
 
 function ImageSlider({ bannerList }) {
     return (
         <div className='w-full h-full'>
             <Swiper
+                modules={[EffectCoverflow]}
+                effect={"coverflow"}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                  }}
                 spaceBetween={10}
                 slidesPerView={1}
                 onSlideChange={() => console.log('slide change')}
