0f715dbb576e8bfcd1b53a475d5161024a8e8c12
Achintha Dilesha <achintha.dilesha@gmail.com>
2022-02-23 16:17:27 +0530

    minor bug fixes

--- src/components/episodes/EpisodeDetailPodcastButton.jsx
+++ src/components/episodes/EpisodeDetailPodcastButton.jsx
@@ -7,6 +7,7 @@ function EpisodeDetailPodcastButton({
   buttonIcon,
   isDark,
   isIconOnly,
+  isReactIcon,
 }) {
 
   // ripple buton declaration
@@ -14,7 +15,7 @@ function EpisodeDetailPodcastButton({
 
   return (
     <button ref={ripple} onMouseDown={event} className={`h-[30px] w-[86px] md:w-[140px] md:h-[40px] bg-[#453598] ${isDark && 'bg-transparent'} hover:bg-[#1B1730] flex items-center justify-center p-[3px]`} onClick={buttonPressed}>
-      {buttonIcon}
+      {isReactIcon === true ? buttonIcon : <img src={buttonIcon} alt="" className='h-[24px] w-[24px]' />}
       <p className={`text-[#00D2CB] text-[9px] text-center ml-[5px] md:ml-[10px] ${isIconOnly && 'hidden'}`}>{buttonName}</p>
     </button>
   )
