401a59f87db2805bf899809cc200a7a32724a16c
Achintha Dilesha <achintha.dilesha@gmail.com>
2022-02-24 10:09:10 +0530

    implemented persistent top bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,14 +12,19 @@ import HomeRightContainer from './home/HomeRightContainer';
 function Home({ isopen }) {
     return (
         <BackgroundContainer>
-            {/* top bar */}
-            <Topbar />
-            <div className='w-full h-full flex '>
-                <div className='flex-auto w-4/6 h-full'>
-                    <HomeLeftContainer />
+            <div className='flex flex-col h-screen'>
+                <div className='py-5'>
+                    <Topbar />
                 </div>
-                <div className='bg-[#110E1F] flex-auto w-1/6 h-full hidden md:hidden lg:inline'>
-                    <HomeRightContainer />
+                <div className='flex-1 overflow-y-auto p-5'>
+                    <div className='w-full h-full flex '>
+                        <div className='flex-auto w-4/6 h-full'>
+                            <HomeLeftContainer />
+                        </div>
+                        <div className='bg-[#110E1F] flex-auto w-1/6 h-full hidden md:hidden lg:inline'>
+                            <HomeRightContainer />
+                        </div>
+                    </div>
                 </div>
             </div>
         </BackgroundContainer>
